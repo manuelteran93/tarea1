1008e2f768e5fa4c2d59ca273ba81dee32694c8d
manuelteran93 <manuelteran93@gmail.com>
2022-08-05 11:08:16 -0300

    tarea

---
+++ tarea1.py
@@ -0,0 +1,13 @@
+""" Desde la consola de python, debes de imprimir las siguientes cadenas de texto:
+
+Hola, soy [tu_nombre]
+
+Estoy empezando el curso de python
+
+Espero aprender mucho
+
+Tienes que subir capturas de pantalla en una carpeta comprimida zip. """
+
+print(" Hola,soy [Jesus Teran] ")
+print("Estoy empezando el curso python")
+print("Espero aprender mucho")
